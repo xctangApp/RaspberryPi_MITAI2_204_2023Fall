b9d2c651bcedccf1380af909fd63aff3547db3b1
xctangApp <99854995+xctangApp@users.noreply.github.com>
2023-12-02 20:54:07 -0600

    Create musicFileTest.py

---
+++ Lecture05/musicFileTest.py
@@ -0,0 +1,6 @@
+import pygame
+
+pygame.init()
+
+#drum = pygame.mixer.Sound("/home/pi/gpio_music_box/drum_tom_hi_soft.wav")
+drum = pygame.mixer.Sound("/home/pi/gpio-music-box/samples/drum_tom_mid_hard.wav")
